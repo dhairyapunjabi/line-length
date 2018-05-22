e621825d20da06ad1cc2027850066b37c70435bd
Dhairya Punjabi <dhairyapunjabi@Dhairyas-MacBook-Pro.local>
2018-05-22 17:46:54 +0530

    Test Line class with some more test cases

--- spec/line/line_spec.rb
+++ spec/line/line_spec.rb
@@ -27,5 +27,36 @@ module Line
       p2=Point.new(1,1)
       expect(Line.new(p1,p2).distance).to eq(Line.new(p2,p1).distance)
     end
+    it "Return the length of the line if all coordinates are positive" do
+      p1=Point.new(1,2)
+      p2=Point.new(1,7)
+      expect(Line.new(p1,p2).distance).to eq(5)
+    end
+    it "Return the length of the line if one coordinate is negative and rest are positive" do
+      p1=Point.new(1,2)
+      p2=Point.new(10,-7)
+      expect(Line.new(p1,p2).distance).to be_between(12.72,12.73)
+    end
+    it "Return the length of the line for all -ve coordinates" do
+      p1=Point.new(-9,-140)
+      p2=Point.new(-67,-7)
+      expect(Line.new(p1,p2).distance).to be_between(145.09,145.1)
+    end
+    it "Return 0 if both the coordinates are the same" do
+      p1=Point.new(1,12)
+      p2=Point.new(1,12)
+      expect(Line.new(p1,p2).distance).to eq(0)
+    end
+    it "Return the length of the line for 2 negative coordinates and 2 positive coordinates" do
+      p1=Point.new(1,12)
+      p2=Point.new(-2,-5)
+      expect(Line.new(p1,p2).distance).to be_between(17.262,17.263)
+    end
+    it "Return the length of the line for 3 negative coordinates and one positive coordinate" do
+      p1=Point.new(-90,12)
+      p2=Point.new(-83,-5)
+      expect(Line.new(p1,p2).distance).to be_between(18.38,18.39)
+    end
+
   end
 end
