25d04c2e790f3788244e5b60d7a04bec3193fad9
Dhairya Punjabi <dhairyapunjabi@Dhairyas-MacBook-Pro.local>
2018-05-22 17:16:16 +0530

    Test Line class with a pt. at origin and other on x axis

--- spec/line/line_spec.rb
+++ spec/line/line_spec.rb
@@ -7,6 +7,11 @@ module Line
       p2=Point.new(0,1)
       expect(Line.new(p1,p2).distance).to eq(1)
     end
+    it "Return the absolute value of the negative coordinate if 3 coordintes are zero and one is negative" do
+      p1=Point.new(0,0)
+      p2=Point.new(-7,0)
+      expect(Line.new(p1,p2).distance).to eq(7)
+    end
 
   end
 end
